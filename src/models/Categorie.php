f15c84487eb895e55dc6976ee42f494c9dc30da9
Alexandre Cherrier <alexandre.cherrier5@etu.univ-lorraine.fr>
2026-04-30 10:31:24 +0200

    [ADDED] model categorie base

---
+++ src/models/Categorie.php
@@ -0,0 +1,9 @@
+<?php
+namespace MyGiftBox\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Categorie extends Model
+{
+    protected $table = 'categories';
+    protected $primaryKey = 'id';
+}
