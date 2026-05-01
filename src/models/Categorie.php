1cc4a4586c842a615072af77e3434cbada0f5ded
gaetanbs <gaetan.babel-sirguey2@etu.univ-lorraine.fr>
2026-05-01 13:26:01 +0200

    [ADDED] 4a

--- src/models/Categorie.php
+++ src/models/Categorie.php
@@ -7,4 +7,10 @@ class Categorie extends Model
     protected $table = 'categorie';
     protected $primaryKey = 'id';
     public $timestamps = false;
+
+
+    function prestations()
+    {
+        return $this->hasMany(Prestation::class, 'categorie_id', 'id');
+    }
 }
--- src/models/Prestation.php
+++ src/models/Prestation.php
@@ -7,4 +7,9 @@ class Prestation extends Model
     protected $table = 'prestation';
     protected $primaryKey = 'id';
     public $timestamps = false;
+
+    function categorie()
+    {
+        return $this->belongsTo(Categorie::class, 'categorie_id', 'id');
+    }
 }
