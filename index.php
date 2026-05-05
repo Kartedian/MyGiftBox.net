dc196923d71678f49919ff22ca85cc39e4a4d67d
gaetanbs <gaetan.babel-sirguey2@etu.univ-lorraine.fr>
2026-05-05 17:47:17 +0200

    [ADDED] resolution sans ddev

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Slim\Factory\AppFactory;
 use Illuminate\Database\Capsule\Manager as DB;
 
-
 $config = parse_ini_file(__DIR__ . '/conf/confdb.ini');
 if ($config !== false) {
     $db = new DB();
@@ -16,9 +15,12 @@ if ($config !== false) {
     $db->bootEloquent();
 }
 
+$config = parse_ini_file(__DIR__ . '/conf/config.ini');
+
 $app = AppFactory::create();
 
 $app->addRoutingMiddleware();
+$app->setBasePath($config['BasePath']);
 $app->addErrorMiddleware(true, true, true);
 
 
