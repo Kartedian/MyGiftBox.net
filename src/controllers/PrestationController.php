621b06cfdb95413e59d44185d9b2ed036c4b2dbe
gaetanbs <gaetan.babel-sirguey2@etu.univ-lorraine.fr>
2026-05-07 11:19:12 +0200

    [ADDED] Erreur PrestationController

--- src/controllers/PrestationController.php
+++ src/controllers/PrestationController.php
@@ -4,13 +4,21 @@ namespace Dwm\MyGiftBox\controllers;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Dwm\MyGiftBox\models\Prestation;
+use Slim\Exception\HttpBadRequestException;
+use Slim\Exception\HttpInternalServerErrorException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Slim\Exception\HttpNotFoundException;
 
 class PrestationController
 {
     public function __invoke(Request $request, Response $response, array $args): Response
     {
-        $params = $request->getQueryParams();
+        $params = $request->getQueryParams()['id'] ?? null;
 
+        if (is_null($params)) {
+            throw new HttpBadRequestException($request, 'Le paramètre "id" est requis.');
+        }
+   if ($params === null) {
         if (!isset($params['id'])) {
             $html = <<<HTML
             <!DOCTYPE html>
@@ -25,19 +33,24 @@ class PrestationController
             $response->getBody()->write($html);
             return $response->withStatus(400);
         }
+   }
 
-        $id = $params['id'];
-        $prestation = Prestation::find($id);
+        
+        try {
+        $prestation = Prestation::findOrFail($params);
+        } catch (ModelNotFoundException $e) {
+            throw new HttpNotFoundException($request, "Prestation avec id {$params} non trouvée.");
+        }
+        
 
         if ($prestation === null) {
-            $safeId = $id;
             $html = <<<HTML
             <!DOCTYPE html>
             <html>
                 <head><title>Prestation introuvable</title></head>
                 <body>
                     <h1>Prestation introuvable</h1>
-                    <p>Aucune prestation avec l'identifiant {$safeId}.</p>
+                    <p>Aucune prestation avec l'identifiant {$params}.</p>
                 </body>
             </html>
             HTML;
