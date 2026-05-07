1dab661e0e8e64489a965c370d0782184449c477
Alexandre Cherrier <alexandre.cherrier5@etu.univ-lorraine.fr>
2026-05-07 11:20:50 +0200

    [added] throw CategorieDetailController

--- src/controllers/CategorieDetailController.php
+++ src/controllers/CategorieDetailController.php
@@ -4,29 +4,25 @@ namespace Dwm\MyGiftBox\controllers;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Dwm\MyGiftBox\models\Categorie;
+use Slim\Exception\HttpInternalServerErrorException;
+use Slim\Exception\HttpBadRequestException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class CategorieDetailController
 {
     public function __invoke(Request $request, Response $response, array $args): Response
     {
         $id = $args['id'];
-        $categorie = Categorie::find($id);
-
-        if ($categorie === null) {
-            $html = <<<HTML
-            <!DOCTYPE html>
-            <html>
-                <head><title>Catégorie introuvable</title></head>
-                <body>
-                    <h1>Catégorie introuvable</h1>
-                    <p>Aucune catégorie avec l'identifiant {$id}.</p>
-                </body>
-            </html>
-            HTML;
-            $response->getBody()->write($html);
-            return $response->withStatus(404);
+        if($id === null){
+            throw new HttpBadRequestException($request, "Le paramètre 'id' est requis pour accéder à cette ressource.");
         }
 
+        try{
+            $categorie = Categorie::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            throw new HttpInternalServerErrorException($request, "Erreur lors de la récupération de la catégorie avec l'identifiant {$id}.");
+        }
+        
         $libelle = $categorie->libelle;
         $description = $categorie->description;
 
