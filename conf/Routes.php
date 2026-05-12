7840f182ac1c8e378b791c4cd613f741a3fff9cf
viper <walid.bouaoukel2@etu.univ-lorraine.fr>
2026-05-13 00:39:30 +0200

    [feature] url_for

--- conf/Routes.php
+++ conf/Routes.php
@@ -11,8 +11,10 @@ use Dwm\MyGiftBox\controllers\AccueilController;
 return function (\slim\App $app): \Slim\App {
     $app->get('[/]', AccueilController::class)->setName('accueil');
     $app->get('/categories[/]', CategorieListeController::class)->setName('categories');
-    $app->get('/categorie/{id}[/]', CategorieDetailController::class)->setName('categorie');
-    $app->get('/prestation[/]', PrestationController::class)->setName('prestation');
+    $app->get('/categorie/{id}[/]', CategorieDetailController::class)->setName('categorie_detail');
+    $app->get('/prestations[/]', PrestationListeController::class)->setName('prestations');
+    $app->get('/prestation/{id}[/]', PrestationDetailController::class)->setName('prestation_detail');
+    $app->get('/categorie/{id:\d+}/prestations[/]', CategoriePrestationListeController::class)->setName('categ2presta');
 
     return $app;
 };
