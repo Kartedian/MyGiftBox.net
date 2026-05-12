1dbf1d82cc929cfb7f19a92678d8287c8d8fda0b
Alexandre Cherrier <alexandre.cherrier5@etu.univ-lorraine.fr>
2026-05-12 18:10:21 +0200

    [ADDED] start link gen

--- conf/Routes.php
+++ conf/Routes.php
@@ -7,10 +7,10 @@ use Dwm\MyGiftBox\controllers\PrestationController;
 use Dwm\MyGiftBox\controllers\AccueilController;
 
 return function (\slim\App $app): \Slim\App {
-    $app->get('[/]', AccueilController::class);
-    $app->get('/categories[/]', CategorieListeController::class);
-    $app->get('/categorie/{id}[/]', CategorieDetailController::class);
-    $app->get('/prestation[/]', PrestationController::class);
+    $app->get('[/]', AccueilController::class)->setName('accueil');
+    $app->get('/categories[/]', CategorieListeController::class)->setName('categories');
+    $app->get('/categorie/{id}[/]', CategorieDetailController::class)->setName('categorie');
+    $app->get('/prestation[/]', PrestationController::class)->setName('prestation');
 
     return $app;
 };
