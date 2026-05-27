d1195c48cccd3254e29e8dcb0e0ce49fab6a5c06
Alexandre Cherrier <alexandre.cherrier5@etu.univ-lorraine.fr>
2026-05-27 16:08:07 +0200

    [feature] add docblocks for CSRF token generation and validation methods

--- gift.appli/src/webui/provider/CsrfTokenProvider.php
+++ gift.appli/src/webui/provider/CsrfTokenProvider.php
@@ -3,6 +3,11 @@ namespace Dwm\MyGiftBox\webui\provider;
 
 class CsrfTokenProvider
 {
+    /**
+     * Génère un token CSRF sécurisé et le stocke en session.
+     * @return string Le token généré
+     * @throws \Exception Si une erreur survient lors de la génération du token
+     */
     public static function generateToken(): string
     {
         $token = bin2hex(random_bytes(32));
@@ -10,6 +15,11 @@ class CsrfTokenProvider
         return $token;
     }
 
+    /**
+     * Valide le token CSRF fourni par rapport à celui stocké en session.
+     * @param string|null $token Le token à valider
+     * @return bool true si le token est valide, false sinon
+     */
     public static function validateToken(?string $token): bool
     {
         if (empty($token) || !isset($_SESSION['csrf_token'])) {
