9918d71c04cd6f3f782ec36cd1e74800dab74fc2
viper <walid.bouaoukel2@etu.univ-lorraine.fr>
2026-05-27 16:07:44 +0200

    [feature] implementation HasUuids

--- gift.appli/src/application_core/application/usecases/BoxService.php
+++ gift.appli/src/application_core/application/usecases/BoxService.php
@@ -82,36 +82,14 @@ class BoxService implements BoxServiceInterface
         }
     }
 
-
-    // TODO: Vérrifier la lib elloquante + ajout verif uid
-    private function generateUuidV4(): string
-    {
-        $data = random_bytes(16);
-        $data[6] = chr((ord($data[6]) & 0x0f) | 0x40);
-        $data[8] = chr((ord($data[8]) & 0x3f) | 0x80);
-    
-        $hex = bin2hex($data);
-    
-        return sprintf(
-            '%s-%s-%s-%s-%s',
-            substr($hex, 0, 8),
-            substr($hex, 8, 4),
-            substr($hex, 12, 4),
-            substr($hex, 16, 4),
-            substr($hex, 20, 12)
-        );
-    }
-
     public function createBox(string $libelle, ?string $description, bool $kdo, ?string $message_kdo, ?string $createurId): BoxEntity
     {
         try {
-            $id = $this->generateUuidV4();
             $token = $this->genererToken(null);
 
             $now = date('Y-m-d H:i:s');
 
             $box = Box::create([
-                'id' => $id,
                 'token' => $token,
                 'libelle' => $libelle,
                 'description' => $description,
