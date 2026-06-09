e5af9fbd11214f2181ccf1b0d9cde366087617f6
viper <walid.bouaoukel2@etu.univ-lorraine.fr>
2026-06-09 10:40:55 +0200

    [added] ThemeDetailAction.php

---
+++ gift.appli/src/webui/actions/ThemeDetailAction.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Dwm\MyGiftBox\webui\actions;
+
+use Dwm\MyGiftBox\application_core\application\usecases\CatalogueServiceInterface;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Exception\HttpInternalServerErrorException;
+use Slim\Exception\HttpNotFoundException;
+use Slim\Views\Twig;
+
+class ThemeDetailAction
+{
+    public function __construct(private readonly CatalogueServiceInterface $catalogue){}
+
+    public function __invoke(Request $request, Response $response, array $args){
+        $id = (int) $args['id'];
+
+        try{
+            $theme = $this->catalogue->getThemeById($id);
+        } catch (EntityNotFoundException $e) {
+            throw new HttpNotFoundException($request, $e->getMessage());
+        } catch (CatalogueException $e) {
+            throw new HttpInternalServerErrorException($request, $e->getMessage());
+        }
+
+        $view = Twig::fromRequest($request);
+        return $view->render($response, 'ThemeDetailView.html', $theme);
+    }
+}
